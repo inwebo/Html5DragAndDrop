f06875d13c1783a31b3958be3afc9adafbc60323
inwebo <inwebo@gmail.com>
2015-04-04 16:16:28 +0200

    Inject event.name

--- core/upload/display/display.js
+++ core/upload/display/display.js
@@ -1,30 +1,24 @@
 //<![CDATA[
 ;(function(window){
 
-
-
     window.LibreJs.Upload.Display = function(list, event){
         var plugin   = this;
         plugin.list  = list;
+        /**
+         * @todo : Custom event
+         */
         plugin.event = event;
 
         var init = function() {
-
-            window.document.addEventListener('dz-queued',function(e){
+            window.document.addEventListener(plugin.event,function(e){
                 for(var item in e.detail) {
                     if(e.detail.hasOwnProperty(item)) {
                         var item = e.detail[item];
                         plugin.list.appendChild(item.element);
-                        console.log('>>>',item, plugin.list);
-
                     }
                 };
-
             }, false);
         };
-
-
-
 
         init();
     };
