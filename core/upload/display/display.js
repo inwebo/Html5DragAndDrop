261b9236c3a9992cee815bf6e6e93bc6136e66c3
inwebo <inwebo@gmail.com>
2015-04-06 00:55:32 +0200

    Typo

--- core/upload/display/display.js
+++ core/upload/display/display.js
@@ -4,11 +4,7 @@
     window.LibreJs.Upload.Display = function(list, event){
         var plugin   = this;
         plugin.list  = list;
-        /**
-         * @todo : Custom event
-         */
         plugin.event = event;
-
         var init = function() {
             window.document.addEventListener(plugin.event,function(e){
                 for(var item in e.detail) {
@@ -22,6 +18,5 @@
 
         init();
     };
-    var Display = window.LibreJs.Upload.Display.prototype.constructor;
 })(window);
 //]]>
